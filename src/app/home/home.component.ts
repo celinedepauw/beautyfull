f895f00194b7af95d27547e26fdba1482d61bc16
celin <celin@localhost>
2022-02-07 11:44:11 +0100

    dynamic categories in home component and router link to go to category component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product, products } from 'src/data/data';
+import { Category, Product, products } from 'src/data/data';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +10,13 @@ import { Product, products } from 'src/data/data';
 export class HomeComponent implements OnInit {
 
   products: Product[] = products
-  
+  categories!:  string[]
+
   constructor() { }
 
   ngOnInit(): void {
+    const cat: string[] = [];
+    Object.values(Category).forEach((value => cat.push(value)));
+    this.categories = cat;
   }
-
 }
